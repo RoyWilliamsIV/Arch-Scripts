bc20a01ee061a8c44b13eda90c8973f347192247
Roy Williams IV <RoyWilliamsIV@users.noreply.github.com>
2018-01-20 13:57:49 -0600

    Update archpad-install.sh
    Issues with gdisk

--- archpad-install.sh
+++ archpad-install.sh
@@ -6,22 +6,24 @@ timedatectl set-ntp true
 # disk partitioning
 (
 echo o    # Create a new empty GPT partition table
+echo y    # Confirm changes
 echo n    # 1. Create BIOS partition
 echo 1    # Partition number
-echo      # First sector (Accept default: 1)
+echo ""    # First sector (Accept default: 1)
 echo +1M  # Last sector (Create 1MB size)
 echo EF02 # Change type to BIOS
 echo n    # 2. Create swap partition
 echo 2    # Partition number
-echo      # First sector (Accept default: 1)
+echo ""    # First sector (Accept default: 1)
 echo +4G  # Last sector (Create 4GB size)
 echo 8200 # Change type to BIOS
 echo n    # 3. Create main partition
 echo 3    # Partition number
-echo      # First sector (Accept default: 1)
-echo      # Last sector (Accept default: varies)
+echo ""     # First sector (Accept default: 1)
+echo ""     # Last sector (Accept default: varies)
 echo EF02 # Change type to BIOS
 echo w    # Write changes
+echo y    # Confirm changes
 ) | gdisk /dev/sda 
 
 # format main partition as ext4
@@ -47,6 +49,7 @@ hwclock --systohc
 
 # set locale
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+
 locale-gen
 
 # set language
