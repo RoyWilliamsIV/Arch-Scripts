b12aa2823913dafd8692f08e8a02a8371d65f191
Roy Williams IV <RoyWilliamsIV@users.noreply.github.com>
2018-01-20 16:04:21 -0600

    re-combine into one

--- archpad-install.sh
+++ archpad-install.sh
@@ -29,3 +29,48 @@ echo y    # Confirm changes
 # format main partition as ext4
 yes | mkfs.ext4 /dev/sda3
 
+# mount main partition
+mount /dev/sda3 /mnt
+
+# start main installation
+pacstrap /mnt base
+
+# generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# enter chroot
+arch-chroot /mnt &
+
+# set time zone
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+
+# sync hardware clock
+hwclock --systohc
+
+# set locale
+
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+
+locale-gen
+
+# set language
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+
+# set hostname
+touch /etc/hostname
+echo 'archpad' >> /etc/hostname
+
+# install network utilites
+pacman -S iw wpa_supplicant dialog
+
+# install intel-ucode and grub packages
+pacman -S intel-ucode grub
+
+# install grub
+grub-install /dev/sda
+
+# generate config file
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# remind user to set new password
+echo "Install finished - please remember to set new root password using passwd."
