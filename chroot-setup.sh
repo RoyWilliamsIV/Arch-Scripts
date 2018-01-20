681225940506c94db8d610353b0744f655835b45
Roy Williams IV <RoyWilliamsIV@users.noreply.github.com>
2018-01-20 14:29:50 -0600

    Create chroot-setup.sh

---
+++ chroot-setup.sh
@@ -0,0 +1,23 @@
+# generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# enter chroot
+arch-chroot /mnt
+
+# set time zone
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+
+# sync hardware clock
+hwclock --systohc
+
+# set locale
+
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+
+locale-gen
+
+# set language
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+
+# set hostname
+echo 'archpad' >> /etc/hostname
