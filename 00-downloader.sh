148b329c62b230f1356298e496f6d642d23bf3d9
Roy Williams IV <RoyWilliamsIV@users.noreply.github.com>
2018-01-20 14:54:49 -0600

    Update 00-downloader.sh

--- 00-downloader.sh
+++ 00-downloader.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-wget 
+wget https://raw.githubusercontent.com/RoyWilliamsIV/arch-install-t60/master/01-pre-install.sh
+wget https://raw.githubusercontent.com/RoyWilliamsIV/arch-install-t60/master/02-pacstrap-install.sh
+wget https://raw.githubusercontent.com/RoyWilliamsIV/arch-install-t60/master/03-chroot-setup.sh
+wget https://raw.githubusercontent.com/RoyWilliamsIV/arch-install-t60/master/04-post-install.sh
+wget https://raw.githubusercontent.com/RoyWilliamsIV/arch-install-t60/master/run-all.sh
