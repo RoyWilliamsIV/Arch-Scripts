9371099147478e82ea62df36448a4cb17094ba40
Roy Williams IV <RoyWilliamsIV@users.noreply.github.com>
2018-01-22 10:34:03 -0600

    Added useradd (probably done) and started sudo

--- archpad-setup.sh
+++ archpad-setup.sh
@@ -7,3 +7,14 @@
 #####################################################################################
 # Roy Williams IV - 2018 - GPLv3 #
 ##################################
+
+# Create User "roy" in wheel Usergroup, prompt user for password.
+useradd -m -G wheel -s /bin/bash roy
+passwd roy
+
+# Install Sudo
+yes | pacman -S sudo
+
+# Configure Sudo for wheel Usergroup
+echo "# Allow all wheel usergroup sudo access
+%wheel  ALL=(ALL) ALL" >> 
