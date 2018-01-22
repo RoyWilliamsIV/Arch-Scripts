0f2caaee8851949cdda560f59947d41efb7ec0ab
Roy Williams IV <RoyWilliamsIV@users.noreply.github.com>
2018-01-22 11:58:14 -0600

    Added swap config

--- archpad-setup.sh
+++ archpad-setup.sh
@@ -8,6 +8,11 @@
 # Roy Williams IV - 2018 - GPLv3 #
 ##################################
 
+# Configure swap to be loaded on startup
+mkswap /dev/sda2
+swapon /dev/sda2
+echo "UUID=$(lsblk -no UUID /dev/sda2) none swap defaults 0 0" >> /etc/fstab
+
 # Create User "roy" in wheel Usergroup, prompt user for password.
 useradd -m -G wheel -s /bin/bash roy
 passwd roy
@@ -18,3 +23,5 @@ yes | pacman -S sudo
 # Configure Sudo for wheel Usergroup
 echo "# Allow all wheel usergroup sudo access
 %wheel  ALL=(ALL) ALL" >> /etc/sudoers
+
+
