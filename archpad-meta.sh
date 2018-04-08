8f2eaab99a896592c06ff31cb7e9230c8590e70c
Roy Williams IV <RoyWilliamsIV@users.noreply.github.com>
2018-04-08 18:27:26 -0500

    Update archpad-meta.sh

--- archpad-meta.sh
+++ archpad-meta.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # This script simply downloads a folder full of the scripts in this repo.
 
-GIT="https://raw.githubusercontent.com/RoyWilliamsIV/Arch-Scripts/master/"  # Git path variable
-
-mkdir "ArchPad Scripts" ; cd "Archpad Scripts" # Make folder for scripts and enter it
-
 wget "$GIT/archpad-install.sh" # Download scripts
 
 echo "## Downloading complete. Use them wisely. ##"
